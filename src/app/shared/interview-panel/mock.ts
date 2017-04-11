e9e49900a2dc4e43d0e42a839a3a47321fe59769
Ashutosh <ashu.singh212@gmail.com>
2017-04-11 19:56:31 +0530

    completed milestone 1

---
+++ src/app/shared/interview-panel/mock.ts
@@ -0,0 +1,13 @@
+// # test-taker should open the link and start solving in 5 min since the link is generated. 
+
+// start_in = 5
+
+// [[problem]]
+// question = "** What is your favorite programming language and why it’s better than other
+// languages? **"
+// time_limit = 10 # test-taker has at most 15 min to solve this problem.
+// [[problem]]
+// question = """
+// ** Given n non-negative integers representing an elevation map where the width of each bar is 1, compute how much water it is able to trap after raining. **
+// <img src = "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAnsAAADyAQMAAAAP2pHtAAAAA3NCSVQICAjb4U/gAAAA BlBMVEX///8AAABVwtN+AAAACXBIWXMAABYlAAAWJQFJUiTwAAAA70lEQVR4nO3bwQ3CIBiGYRCiRwfw0FHKZjKaozB CN0BTTerBNCF+JD/6vte2T+hPLxzqHBH9Sl4NBjmYxeBJDt7E4FkOFjE4qcF5MQ8WMaj/DgEBAQEBAQEBAQEBAQEBhw eTeTCbB1s2xde1/ZtaZhie4P4D9sGWGQICAgICAgICAgICAg4J2j+ajXnWm2pjpSN4UINHNRjNzxAQEBAQEBAQEBAQ8 N/AoAa9+VfuB17ersytYF3c9qPLC2xHPqobeBWBPotXKAf7zRAQEBAQEBAQEBAQENAIGJP1FT7A76XOYNCCMTltcjCI PSIiWrsDYz85NQG0QW8AAAAASUVORK5CYII=">"""
+// time_limit = 30
